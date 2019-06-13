81e1ef0da8cc40db55cb462fcd8a35f2f993d1f5
wangfei05 <wangfei05@hexindai.com>
2019-06-13 15:41:14 +0800

    添加websocket相关内容

--- src/main/java/com/ty/web/WebSocketController.java
+++ src/main/java/com/ty/web/WebSocketController.java
@@ -1,5 +1,6 @@
 package com.ty.web;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
